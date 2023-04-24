34dd25a1bf7878e75122aeec2bed77b38fbe1d67
Ledamor <Leandros2000@gmail.com>
2023-04-25 02:02:18 +0300

    fixed enter refresh bug

--- twitter-app/src/components/TweetBox/TweetBox.tsx
+++ twitter-app/src/components/TweetBox/TweetBox.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 import { useState,useEffect } from 'react';
 import TweetComponent from '../Tweet/TweetComponent';
 import axios from 'axios';
-
-
 
 const TweetBox =(props: { color: string; }) => {
   const [messages, setMessages] = useState<string[]>([]);
@@ -15,10 +13,32 @@ const TweetBox =(props: { color: string; }) => {
     setinputMessage(event.target.value);
   };
   
-  const onTweetClick = () => {
+  const onFormSubmit = (event: React. FormEvent) => {
      setMessages(oldMessages => [...oldMessages , inputMessage])
-     }
+     event.preventDefault()
+     
+     } 
+     const onFormClick = (event: React. MouseEvent<HTMLElement>) => {
+      setMessages(oldMessages => [...oldMessages , inputMessage])
+      
+      
+      } 
+       
+      
+  
 
+  const getoldMsgs = () => {
+    axios({
+      url: 'localhost:5000/members',
+      // configuration
+  })
+  .then(response => {
+      return messages
+
+  })
+
+  }
+ 
   useEffect(() => {
     const sendingData = async () => {  
         const data = { messages };
@@ -37,17 +57,18 @@ const TweetBox =(props: { color: string; }) => {
         <div>
 
           <h3 style = { {color: props.color} }> Tweet here </h3>
-          <form onSubmit={onTweetClick}>
+          <form onSubmit={onFormSubmit}>
             <input type="text" 
             name = "message" 
             id = "message"
             onChange={handleChange}
             className="Input-box" />
-           < img onClick = {onTweetClick}  style={{
+           < img onClick = {onFormClick}  style={{
           width: 26,
           height: 15 }} src="https://as2.ftcdn.net/v2/jpg/05/30/79/15/1000_F_530791557_T8hNharBQQJqmw0R2FRjCrC9CgNwaeui.webp"/>
          </form>
           </div>
+          
         
       <br></br>
       
