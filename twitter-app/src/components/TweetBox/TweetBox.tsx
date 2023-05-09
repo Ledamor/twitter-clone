0d442a1b36d6087e1498eb8d161dab3447d34083
Ledamor <Leandros2000@gmail.com>
2023-05-10 01:04:07 +0300

    finally fixed refresh doublication problem

--- twitter-app/src/components/TweetBox/TweetBox.tsx
+++ twitter-app/src/components/TweetBox/TweetBox.tsx
@@ -1,95 +1,91 @@
 import React, { FormEvent } from 'react';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import TweetComponent from '../Tweet/TweetComponent';
 import axios from 'axios';
 
-  const TweetBox =(props: { color: string; }) => {
+const TweetBox = (props: { color: string; }) => {
   const [messages, setMessages] = useState<string[]>([]);
-  
-  const [inputMessage, setinputMessage] = useState('');
+  const [numberOfTweets, setNumberOfTweets] = useState(0);
 
+  useEffect(() => {
+    setNumberOfTweets(messages.length);
+  }, [messages])
+
+  const [inputMessage, setinputMessage] = useState('');
   const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
     // 👇 Get input value from "event"
     setinputMessage(event.target.value);
   };
-  
-  const onFormSubmit = (event: React. FormEvent) => {
-     setMessages(oldMessages => [...oldMessages , inputMessage])
-     event.preventDefault()
-     
-     } 
-  const onFormClick = (event: React. MouseEvent<HTMLElement>) => {
-  setMessages(oldMessages => [...oldMessages , inputMessage])
-      
-      
-      } 
-       
-      
 
+  const onFormSubmit = (event: React.FormEvent) => {
+    setMessages(oldMessages => [...oldMessages, inputMessage])
+    console.log("onFormSubmit", messages)
+    event.preventDefault()
 
-    useEffect(() => {
-      const receivingData = async () => {  
-        const response = await axios.get('http://localhost:5000/members');
-        console.log(response.data);
-          setMessages(oldMessages => [...oldMessages , ...response.data.members])
-      }
-        
-          receivingData()
-          .catch(console.error);
-        }, []);
-          
-          
-          
-    
-  
- 
+  }
+  const onFormClick = (event: React.MouseEvent<HTMLElement>) => {
+
+    setMessages(oldMessages => [...oldMessages, inputMessage])
+    console.log("onFormClick", messages)
+  }
+
   useEffect(() => {
-    const sendingData = async () => {  
-        const data = { messages };
-        const response = await axios.post('http://localhost:5000/save-data', data);
-        console.log(response.data);
-      }
+    const receivingData = async () => {
+
+      const response = await axios.get('http://localhost:5000/members');
+
+      console.log('receivingData', messages)
+      setMessages(oldMessages => [...response.data.members])
+
+    }
+
+    
+    receivingData()
+      .catch(console.error);
+  }, []);
+
+  useEffect(() => {
+      if (messages.length == 0) {return } 
+      const sendingData = async () => {
+      
+      const data = { messages };
+      const response = await axios.post('http://localhost:5000/save-data', data);
+      
+      console.log('sendingData44444', messages)
+    }
     sendingData()
-          .catch(console.error);
-}, [messages]);
+      .catch(console.error);
+  }, [messages]);
 
-
-
-    
-  
-
-  
-  
-    
 
   return (
     <div  >
-        <div>
+      <div>
 
-          <h3 style = { {color: props.color} }> Tweet here </h3>
-          <form onSubmit={onFormSubmit}>
-            <input type="text" 
-            name = "message" 
-            id = "message"
+        <h3 style={{ color: props.color }}> Tweet here </h3>
+        <form onSubmit={onFormSubmit}>
+          <input type="text"
+            name="message"
+            id="message"
             onChange={handleChange}
             className="Input-box" />
-           < img onClick = {onFormClick}  style={{
-          width: 26,
-          height: 15 }} src="https://as2.ftcdn.net/v2/jpg/05/30/79/15/1000_F_530791557_T8hNharBQQJqmw0R2FRjCrC9CgNwaeui.webp"/>
-         </form>
-          </div>
-          
-        
+          < img onClick={onFormClick} style={{
+            width: 26,
+            height: 15
+          }} src="https://as2.ftcdn.net/v2/jpg/05/30/79/15/1000_F_530791557_T8hNharBQQJqmw0R2FRjCrC9CgNwaeui.webp" />
+        </form>
+      </div>
       <br></br>
-      
-     
-     {messages.map(tweet => <TweetComponent TweetMessage={tweet}/>) }
-     
-     <br></br>
+       
+
+
+      {messages.map(tweet => <TweetComponent TweetMessage={tweet} />)}
+
+      <br></br>
 
     </div>
-    
+
   );
-  }
+}
 
 export default TweetBox;
